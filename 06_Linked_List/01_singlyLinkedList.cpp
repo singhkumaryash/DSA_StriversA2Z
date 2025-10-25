f7a723f61f4bda7bb073b71006d3811c21b0830d
Yash Kumar Singh <yashkumarsingh1607@gmail.com>
2025-10-25 20:27:09 +0530

    Added more functions

--- 06_Linked_List/01_singlyLinkedList.cpp
+++ 06_Linked_List/01_singlyLinkedList.cpp
@@ -14,6 +14,7 @@ public:
         this->data = data;
         this->next = nullptr;
     }
+
     // Constructor to initialize a node with next pointer
     Node(int data, Node *next)
     {
@@ -44,17 +45,107 @@ public:
             delete temp;
         }
     }
+
     // Function to insert at head
     void insertAtHead(int data)
     {
         Node *newNode = new Node(data);
-        Node *temp = head;
+        newNode->next = head;
         head = newNode;
-        newNode->next = temp;
+    }
+
+    // Function to insert at tail
+    void insertAtTail(int data)
+    {
+        Node *newNode = new Node(data);
+        if (head == nullptr)
+        {
+            head = newNode;
+            return;
+        }
+        else
+        {
+            Node *temp = head;
+            while (temp->next != nullptr)
+            {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+    }
+
+    // Function to insert value at a given psoition K
+    void insertAtK(int value, int k)
+    {
+        if (k < 1)
+        {
+            cout << "Invalid Index!" << endl;
+            return;
+        }
+        if (k == 1)
+        {
+            insertAtHead(value);
+            return;
+        }
+        int count = 1;
+        Node *temp = head;
+        while (count < k - 1 && temp != nullptr)
+        {
+            temp = temp->next;
+            count++;
+        }
+        if (temp == nullptr)
+        {
+            cout << "Index out of bound!" << endl;
+            return;
+        }
+        Node *newNode = new Node(value);
+        newNode->next = temp->next;
+        temp->next = newNode;
+    }
+
+    // Function to insert before a given value
+    void insertBeforeValue(int value, int key)
+    {
+        if (head == nullptr)
+        {
+            cout << "List is Empty!" << endl;
+            return;
+        }
+        if (head->data == key)
+        {
+            insertAtHead(value);
+            return;
+        }
+        Node *temp = head;
+        while (temp->next != nullptr && temp->next->data != key)
+        {
+            temp = temp->next;
+        }
+        if (temp->next == nullptr)
+        {
+            cout << "Key not found!" << endl;
+            return;
+        }
+        Node *newNode = new Node(value, temp->next);
+        temp->next = newNode;
+    }
+
+    // Function to delete the head node
+    void removeHead()
+    {
+        if (head == nullptr)
+        {
+            cout << "List is Empty~" << endl;
+            return;
+        }
+        Node *temp = head;
+        head = head->next;
+        delete temp;
     }
 
     // Function to delete the tail node
-    void deleteTail()
+    void removeTail()
     {
         // if list is empty
         if (head == nullptr)
@@ -79,6 +170,74 @@ public:
         temp->next = nullptr;
     }
 
+    // Function to delete Kth element from linked list
+    void removeKthElement(int k)
+    {
+        if (head == nullptr)
+        {
+            cout << "List is Empty!" << endl;
+            return;
+        }
+        if (k < 1)
+        {
+            cout << "Invalid Index!" << endl;
+        }
+
+        if (k == 1)
+        {
+            removeHead();
+            return;
+        }
+
+        int count = 1;
+        int index = k - 1;
+        Node *temp = head;
+        while (count < index && temp->next != nullptr)
+        {
+            temp = temp->next;
+            count++;
+        }
+        if (temp->next == nullptr || temp == nullptr)
+        {
+            cout << "Index out of bound!" << endl;
+            return;
+        }
+        Node *nodeToDelete = temp->next;
+        temp->next = temp->next->next;
+        delete nodeToDelete;
+    }
+
+    // Function to remove element by value
+    void removeByValue(int value)
+    {
+        if (head == nullptr)
+        {
+            cout << "List is Empty!" << endl;
+            return;
+        }
+
+        if (head->data == value)
+        {
+            removeHead();
+            return;
+        }
+
+        Node *temp = head;
+        while (temp->next != nullptr && temp->next->data != value)
+        {
+            temp = temp->next;
+        }
+        if (temp->next == nullptr)
+        {
+            cout << "Value not found!" << endl;
+            return;
+        }
+
+        Node *nodeToDelete = temp->next;
+        temp->next = nodeToDelete->next;
+        delete nodeToDelete;
+    }
+
     // Function to display the linked list
     void display()
     {
@@ -101,7 +260,7 @@ public:
         }
     }
 
-    // Function to get lenght of linked list
+    // Function to get length of linked list
     int lengthOfLinkedList()
     {
         int lenght = 0;
@@ -134,10 +293,16 @@ int main()
     sll.insertAtHead(90);
     sll.insertAtHead(67);
     sll.insertAtHead(78);
-    sll.deleteTail();
+    sll.removeTail();
     sll.display();
     cout << (sll.lengthOfLinkedList()) << endl;
     cout << (sll.search(0)) << endl;
     cout << (sll.search(90)) << endl;
+    sll.removeByValue(1);
+    sll.display();
+    sll.insertAtK(100, 2);
+    sll.display();
+    sll.insertBeforeValue(500, 90);
+    sll.display();
     return 0;
 }
